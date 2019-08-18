af41654a6cbf88df4562396f531dab338c7187b8
wangdunwei <wangdunwei@wxbtc.com>
2019-08-18 10:42:46 +0800

    删除androidx模块

--- app/src/main/java/androidx/ayvytr/rados/recyclerviewlibrary/Item.kt
+++
@@ -1,7 +0,0 @@
-package androidx.ayvytr.rados.recyclerviewlibrary
-
-data class Item(var value: String, val type: Int) {
-    override fun toString(): String {
-        return "Item(value='$value', type=$type)"
-    }
-}
