f0f1612de33529d2549529037a1449e1fbcb4757
ayvytr <ayvytr@outlook.com>
2025-12-30 16:00:35 +0800

    1.更新项目，只保留androidx模块 2.修改项目配置

--- androidx-app/src/main/java/com/ayvytr/demo/MainActivity.kt
+++ androidx-app/src/main/java/com/ayvytr/demo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ayvytr.demo
 
+import android.R.attr.text
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,17 +9,16 @@ import com.ayvytr.adapter.SmartContainer
 import com.ayvytr.adapter.SmartDiffCallback
 import com.ayvytr.adapter.smart
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.ayvytr.demo.databinding.ActivityMainBinding
 import com.ayvytr.ktx.context.toast
 import com.ayvytr.ktx.ui.getContext
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.item.view.*
-import kotlinx.android.synthetic.main.item_type3.view.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var smartAdapter: SmartAdapter<Item>
     val list = listOf(Item("first", 1), Item("second", 2), Item("third", 1), Item("fourth", 0), Item("fifth", 1),
                       Item("third", 1), Item("fourth", 0), Item("fifth", 2))
+    private val binding by viewBinding<ActivityMainBinding>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,14 +31,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun initView2() {
         smartAdapter = smart(list, R.layout.item, {
-            item_text.text = it.value
+            findViewById<TextView>(R.id.item_text).text = it.value
         }) {
             multiItemViewOf = listOf(SmartContainer(R.layout.item_type2, 1) {
                 val tv = findViewById<TextView>(R.id.tv2)
                 tv.text = it.value
 //                item_text.text = it.value
             }, SmartContainer(R.layout.item_type3, 2) {
-                tv3.text = it.value
+                findViewById<TextView>(R.id.tv3).text = it.value
 //                item_text.text = it.value
             })
             type = { it.type }
@@ -56,14 +56,14 @@ class MainActivity : AppCompatActivity() {
         smartAdapter = smart<Item> {
             items = list
             itemViewOf = SmartContainer(R.layout.item, 0) {
-                item_text.text = it.value
+                findViewById<TextView>(R.id.item_text).text = it.value
             }
             multiItemViewOf = listOf(SmartContainer(R.layout.item_type2, 1) {
                 val tv = findViewById<TextView>(R.id.tv2)
                 tv.text = it.value
 //                item_text.text = it.value
             }, SmartContainer(R.layout.item_type3, 2) {
-                tv3.text = it.value
+                findViewById<TextView>(R.id.tv3).text = it.value
 //                item_text.text = it.value
             })
             type = { it.type }
@@ -79,97 +79,101 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initAdapter() {
-        recycler_view.layoutManager = LinearLayoutManager(getContext())
-        recycler_view.adapter = smartAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(getContext())
+        binding.recyclerView.adapter = smartAdapter
     }
 
     private fun initClick() {
-        btn_add.setOnClickListener {
-            val index = if (smartAdapter.isEmpty()) 0 else Random.nextInt(smartAdapter.itemCount)
-            smartAdapter.add(itemCreator(), index)
-        }
+        binding.apply {
 
-        btn_remove_at.setOnClickListener {
-            if (smartAdapter.isEmpty()) {
-                return@setOnClickListener
+            btnAdd.setOnClickListener {
+                val index =
+                    if (smartAdapter.isEmpty()) 0 else Random.nextInt(smartAdapter.itemCount)
+                smartAdapter.add(itemCreator(), index)
             }
 
-            if (smartAdapter.itemCount == 0) {
-                smartAdapter.removeAt(0)
-            } else {
-                smartAdapter.removeAt(Random.nextInt(smartAdapter.itemCount))
-            }
-        }
+            btnRemoveAt.setOnClickListener {
+                if (smartAdapter.isEmpty()) {
+                    return@setOnClickListener
+                }
 
-        btn_remove.setOnClickListener {
-            if (smartAdapter.isEmpty()) {
-                return@setOnClickListener
-            }
-
-            val item = smartAdapter.list.firstOrNull { it.value == "Creator" }
-            if (item != null) {
-                smartAdapter.remove(item)
-                toast("删除${item}成功")
-            } else {
-                toast("未找到!")
-            }
-        }
-
-        btn_addAll.setOnClickListener {
-            val list = mutableListOf<Item>()
-            repeat(3) {
-                list.add(itemCreator())
-            }
-            if (smartAdapter.isEmpty()) {
-                smartAdapter.addAll(list, 0)
-            } else {
-                smartAdapter.addAll(list, Random.nextInt(smartAdapter.itemCount))
-            }
-        }
-
-        btn_clear.setOnClickListener {
-            smartAdapter.clear()
-        }
-
-        btn_remove_3.setOnClickListener {
-            if (smartAdapter.itemCount < 3) {
-                toast("itemCount < 3 !")
-                return@setOnClickListener
+                if (smartAdapter.itemCount == 0) {
+                    smartAdapter.removeAt(0)
+                } else {
+                    smartAdapter.removeAt(Random.nextInt(smartAdapter.itemCount))
+                }
             }
 
-            var index = 0
-            if (smartAdapter.itemCount != 3) {
-                index = Random.nextInt(smartAdapter.itemCount - 3)
-            }
-            val subList = smartAdapter.list.subList(index, index + 3)
-            smartAdapter.removeAll(subList)
-            toast("删除成功")
-        }
+            btnRemove.setOnClickListener {
+                if (smartAdapter.isEmpty()) {
+                    return@setOnClickListener
+                }
 
-        btn_update.setOnClickListener {
-            if (smartAdapter.isEmpty()) {
-                return@setOnClickListener
-            }
-            val copyList = mutableListOf<Item>()
-            smartAdapter.list.forEachIndexed { index, item ->
-                copyList.add(Item("$index Update", item.type))
-            }
-            copyList.forEachIndexed { index, item ->
-                item.value = "$index Update"
+                val item = smartAdapter.list.firstOrNull { it.value == "Creator" }
+                if (item != null) {
+                    smartAdapter.remove(item)
+                    toast("删除${item}成功")
+                } else {
+                    toast("未找到!")
+                }
             }
 
-            smartAdapter.update(copyList)
-            toast("更新成功")
-        }
+            btnAddAll.setOnClickListener {
+                val list = mutableListOf<Item>()
+                repeat(3) {
+                    list.add(itemCreator())
+                }
+                if (smartAdapter.isEmpty()) {
+                    smartAdapter.addAll(list, 0)
+                } else {
+                    smartAdapter.addAll(list, Random.nextInt(smartAdapter.itemCount))
+                }
+            }
 
-        btn_reset.setOnClickListener {
-            smartAdapter.update(list)
-        }
+            btnClear.setOnClickListener {
+                smartAdapter.clear()
+            }
 
-        btn_update_for_append.setOnClickListener {
-            val isAppendToHead = true
-            smartAdapter.update(listOf(randomItem()), true, isAppendToHead)
-            recycler_view.scrollToPosition(if(isAppendToHead) 0 else smartAdapter.itemCount)
+            btnRemove3.setOnClickListener {
+                if (smartAdapter.itemCount < 3) {
+                    toast("itemCount < 3 !")
+                    return@setOnClickListener
+                }
+
+                var index = 0
+                if (smartAdapter.itemCount != 3) {
+                    index = Random.nextInt(smartAdapter.itemCount - 3)
+                }
+                val subList = smartAdapter.list.subList(index, index + 3)
+                smartAdapter.removeAll(subList)
+                toast("删除成功")
+            }
+
+            btnUpdate.setOnClickListener {
+                if (smartAdapter.isEmpty()) {
+                    return@setOnClickListener
+                }
+                val copyList = mutableListOf<Item>()
+                smartAdapter.list.forEachIndexed { index, item ->
+                    copyList.add(Item("$index Update", item.type))
+                }
+                copyList.forEachIndexed { index, item ->
+                    item.value = "$index Update"
+                }
+
+                smartAdapter.update(copyList)
+                toast("更新成功")
+            }
+
+            btnReset.setOnClickListener {
+                smartAdapter.update(list)
+            }
+
+            btnUpdateForAppend.setOnClickListener {
+                val isAppendToHead = true
+                smartAdapter.update(listOf(randomItem()), true, isAppendToHead)
+                recyclerView.scrollToPosition(if (isAppendToHead) 0 else smartAdapter.itemCount)
+            }
         }
     }
 
